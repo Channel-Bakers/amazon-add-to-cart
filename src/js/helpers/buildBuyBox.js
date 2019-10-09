cac5880a8946bab28d6a70c97a06b0dfd26c995e
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-10-09 10:11:07 -0700

    build new buy box for digital products

--- src/js/helpers/buildBuyBox.js
+++ src/js/helpers/buildBuyBox.js
@@ -2,19 +2,32 @@
  * This is for products like Norton that don't generate ATC links on Buy Boxes
  */
 export default async link => {
+
+	if (!link.getAttribute('data-component-type') === 'BuyBoxAddToCart')
+	{
+		// Add the attributes
+		const ASIN = link.closest('div[data-asin]').getAttribute('data-asin');
+
+		link.setAttribute('data-component-id', `${ASIN}_ATC`);
+		link.setAttribute('data-component-type', 'BuyBoxAddToCart');
+
+		// Add the cart icon
+		const BUTTON = link.querySelector('.a-button');
+		const ICON_WRAPPER = link.querySelector('.a-button-inner');
+
+		if (BUTTON)
+			BUTTON.classList.add('a-button-icon');
+
+		const ICON = document.createElement('i');
+		ICON.classList.add('a-icon');
+		ICON.classList.add('a-icon-cart');
 		
-	// Add the attributes
-	const ASIN = link.closest('div[data-asin]').getAttribute('data-asin');
+		if (ICON_WRAPPER)
+			ICON_WRAPPER.prepend(ICON);
 
-	link.setAttribute('data-component-id', ASIN);
-	link.setAttribute('data-component-type', 'BuyBoxAddToCart');
-
-	// Add the cart icon
-	link.querySelector('.a-button').classList.add('a-button-icon');
-	link.querySelector('.a-button-inner').prepend('<i class="a-icon a-icon-cart"></i>');
-
-	// Change the button text
-	link.querySelector('.a-button-text').innerHTML = 'Add to Cart';
+		// Change the button text
+		link.querySelector('.a-button-text').innerHTML = 'Add to Cart';
+	}
 
 	return link;
 };
