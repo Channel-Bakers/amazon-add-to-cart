a8406eb648de38d5c24886655e2d1a63f0d04cea
Ryan DiMascio <ryand@channelbakers.com>
2020-02-04 10:41:01 -0800

    major refactor with mutation observer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+'use strict'
+
+const fs = require('fs')
 const path = require('path')
 const autoprefixer = require('autoprefixer')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebookincubator/create-react-app/issues/637
+const appDirectory = fs.realpathSync(process.cwd())
+
+function resolveApp(relativePath) {
+	return path.resolve(appDirectory, relativePath)
+}
 
 module.exports = {
 	entry: {
